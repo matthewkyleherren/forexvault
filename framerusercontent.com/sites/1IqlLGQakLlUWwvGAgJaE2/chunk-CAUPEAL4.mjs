46cda64828686f61498020e89b28e2f71c96dcd0
Matthew Herren <m@wrk.ltd>
2025-03-18 20:58:04 +0100

    hzjugvzu

--- framerusercontent.com/sites/1IqlLGQakLlUWwvGAgJaE2/chunk-CAUPEAL4.mjs
+++ framerusercontent.com/sites/1IqlLGQakLlUWwvGAgJaE2/chunk-CAUPEAL4.mjs
@@ -1,2 +1,2 @@
-function t(a,e){return{description:"The first compliant RWA yield chain built on and supported by Optimism\u2122",favicon:"https://framerusercontent.com/assets/4K5GpdkoUdDGCrwLtoXEj04Vzg0.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/JjpYpb75TGIMvFAjpV02d1Hk8.jpg",title:"OZEAN \u2014 The Blockchain for Real-World Asset Yields"}}export{t as a};
+function t(a,e){return{description:"The first compliant RWA yield chain built on and supported by Optimism\u2122",favicon:"/assets/media/4K5GpdkoUdDGCrwLtoXEj04Vzg0.png",robots:"max-image-preview:large",socialImage:"/assets/media/JjpYpb75TGIMvFAjpV02d1Hk8.jpg",title:"OZEAN \u2014 The Blockchain for Real-World Asset Yields"}}export{t as a};
 //# sourceMappingURL=chunk-CAUPEAL4.mjs.map
